aeb6f8cfdf548f44c524c4c90d50f33ae8dce263
kanimozhis-4 <kanimozhi808088@gmail.com>
2024-11-08 10:54:03 +0530

    added the folder for drill 1

---
+++ Drill1/Solution.js
@@ -0,0 +1,7 @@
+//    Given the dataset of individuals, write a function that accesses and returns the email addresses of all individuals.
+  
+export function getEmailAddress(Details){  
+    console.log(Details);
+
+
+}
---
+++ Drill1/Test.js
@@ -0,0 +1,4 @@
+import { arrayOfObjects } from "./person";  
+import { arrayOfObjects } from "./person";
+
+const result =getEmailAddress(arrayOfObjects);
